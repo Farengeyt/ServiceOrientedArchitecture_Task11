edd680b14982d4fc0e8dd3d550e728d3cda02064
Farengeyt <Ivantzyo@gmail.com>
2020-05-26 00:48:27 +0300

    Added Task11. Working with Task10

---
+++ WpfWebApiClient/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace WpfWebApiClient.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public float AvgMark { get; set; }
+
+        public Student() { }
+
+        public Student(string name, float avgMark)
+        {
+            Name = name;
+            AvgMark = avgMark;
+        }
+    }
+}
---
+++ WpfWebApiClient/ServiceProvider.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using WpfWebApiClient.Models;
+
+namespace WpfWebApiClient
+{
+    public static class ServiceProvider
+    {
+        private static HttpClient HttpClient = new HttpClient();
+
+        public static async Task<Student[]> GetStudentGratherThan(float mark)
+        {
+            string markString = mark.ToString().Replace(',', '.');
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://localhost:44313/GetStudentGratherThan?mark=" + markString);
+            requestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = await HttpClient.SendAsync(requestMessage).ConfigureAwait(true);
+            string jsonResponse = await response.Content.ReadAsStringAsync().ConfigureAwait(true);
+            Student[] result = await ParseJsonAsync(jsonResponse);
+            requestMessage.Dispose();
+            return result;
+        }
+
+        public static async Task<Student[]> GetStudentLowerThan(float mark)
+        {
+            string markString = mark.ToString().Replace(',', '.');
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://localhost:44313/GetStudentLowerThan?mark=" + markString);
+            requestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = await HttpClient.SendAsync(requestMessage).ConfigureAwait(true);
+            string jsonResponse = await response.Content.ReadAsStringAsync().ConfigureAwait(true);
+            Student[] result = await ParseJsonAsync(jsonResponse);
+            requestMessage.Dispose();
+            return result;
+        }
+
+        public static async Task<Student[]> GetStudentInRange(float minMark, float maxMark)
+        {
+            string minMarkString = minMark.ToString().Replace(',', '.');
+            string maxMarkString = maxMark.ToString().Replace(',', '.');
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, "https://localhost:44313/GetStudentInRange");
+            requestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            requestMessage.Content = new StringContent("{ Item1: " + minMarkString + ", Item2: " + maxMarkString + " }");
+            requestMessage.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = await HttpClient.SendAsync(requestMessage).ConfigureAwait(true);
+            string jsonResponse = await response.Content.ReadAsStringAsync().ConfigureAwait(true);
+            Student[] result = await ParseJsonAsync(jsonResponse);
+            requestMessage.Dispose();
+            return result;
+        }
+        
+        private static async Task<Student[]> ParseJsonAsync(string json)
+        {
+            return await Task.Run(() =>
+            {
+                return JsonConvert.DeserializeObject<Student[]>(json);
+            }).ConfigureAwait(true);
+        }
+
+    }
+}
